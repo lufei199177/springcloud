8f2457b9be2ec7a0edfc6e3ebf84374dbd8329dc
lufei <934578991@qq.com>
2020-12-08 17:43:52 +0800

    swagger功能开发

--- demo/src/main/java/com/springcloud/demo/controller/UserController.java
+++ demo/src/main/java/com/springcloud/demo/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.springcloud.demo.controller;
 
 import com.springcloud.demo.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author lufei
@@ -13,13 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@Api(value = "user",tags = {"UserController"})
 public class UserController {
 
     @GetMapping("/get")
+    @ApiOperation(value = "查询单个用户",response = User.class)
     public User get(){
         User user= new User();
         user.setAge(24);
         user.setName("lufei");
         return user;
+    }
+
+    @PostMapping("/add")
+    @ApiOperation(value = "新增用户",response = String.class)
+    public String add(User user){
+        return "add success";
+    }
+
+    @PostMapping("/update")
+    @ApiOperation(value = "修改用户",response = String.class)
+    public String update(@RequestBody User user){
+        return "update success";
     }
 }
--- demo/src/main/java/com/springcloud/demo/model/User.java
+++ demo/src/main/java/com/springcloud/demo/model/User.java
@@ -1,5 +1,7 @@
 package com.springcloud.demo.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -8,9 +10,13 @@ import lombok.Data;
  * @desc
  */
 @Data
+@ApiModel(value = "User",description = "用户实体类")
 public class User {
 
+    @ApiModelProperty(value = "id")
     private String id;
+    @ApiModelProperty(value = "姓名",required = true)
     private String name;
+    @ApiModelProperty(value = "年龄",required = true)
     private int age;
 }
